112ae2692c38453fdd01330749c667c48c93154d
mholwill <m_holwill@hotmail.co.uk>
2020-11-03 14:25:39 +0000

    added image uploadert

--- client/src/components/MunroDetail.vue
+++ client/src/components/MunroDetail.vue
@@ -1,17 +1,76 @@
 <template>
     <section>
         <h1>{{ munro.name }}</h1>
-        <button>Visited</button>
+        <form action="submit">
+            <label for="date"></label>
+            <input type="date" name="date" id="date" v-model="date">
+            <label for="uploadPhoto"></label>
+            <a class="btn" @click="toggleShow">Upload Image</a>
+            <my-upload field="img"
+                @crop-success="cropSuccess"
+                @crop-upload-success="cropUploadSuccess"
+                @crop-upload-fail="cropUploadFail"
+                langType="en"
+                v-model="show"
+                :width="300"
+                :height="300"
+                url="http://localhost:3000/api/munros/"
+                :params="params"
+                :headers="headers"
+                img-format="png">
+            </my-upload>
+            <img :src="imgDataUrl">
+            
+            <input type="submit">
+        </form>
     </section>
 </template>
 
 <script>
 import {eventBus} from '@/main.js'
+import myUpload from 'vue-image-crop-upload';
 
 export default {
     name: "munro-detail",
-    props: ['munro']
+    props: ['munro'],
+    data() {
+        return {
+            date: '',
+            show: false,
+            params: {
+                token: '123456798',
+                name: 'avatar'
+            },
+            headers: {
+                smail: '*_~'
+            },
+            imgDataUrl: ''
+        }
+    },
+    methods: {
+        toggleShow() {
+            this.show = !this.show;
+        },
+        cropSuccess(imgDataUrl, field){
+            console.log('-------- crop success --------');
+            this.imgDataUrl = imgDataUrl;
+            this.show = false;
+        },
+        cropUploadSuccess(jsonData, field){
+            console.log('-------- upload success --------');
+            console.log(jsonData);
+            console.log('field: ' + field);
+        },
+        cropUploadFail(status, field){
+            console.log('-------- upload fail --------');
+            console.log(status);
+            console.log('field: ' + field);
+        }
 
+    },
+    components: {
+        'my-upload': myUpload
+    }
 }
 </script>
 
